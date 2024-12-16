c20345e5c007234bb1712b2a5dd94ead0c63e2d4
codesamp-rohan <rohanchaudhary.bkbiet2022@gmail.com>
2024-12-16 19:27:31 +0530

    added some more projects

--- src/Components/ProjectList.jsx
+++ src/Components/ProjectList.jsx
@@ -1,21 +1,19 @@
 import cornerImg from "../assets/images/corner.png";
 
-import holesailImg from "../assets/images/holesail.png";
-import deployImg from "../assets/images/deployClone.png";
-import blogImg from "../assets/images/blog.png";
-import sibiroImg from "../assets/images/sibiro.png";
-import P2PImg from "../assets/images/P2PClip.png";
-
 import blogMockupImg from "../assets/images/blogMockup.png";
 import holesailMockupImg from "../assets/images/holesailMockup.png";
 import sibiroMockupImg from "../assets/images/sibiroMockup.png";
 import docyardMockupImg from "../assets/images/docyardMockup.png";
 import deployMockupImg from "../assets/images/deployMockup.png";
 import gpuMockupImg from "../assets/images/GPUMockup.png";
+import P2PTextMockupImg from "../assets/images/P2PTextMockup.png";
+import P2PTaskMockupImg from "../assets/images/P2PTask.png";
+import holesailGoImg from "../assets/images/goMockup.png";
+import P2PImg from "../assets/images/P2P.png";
 
 const ProjectList = () => {
     return(
-        <div className="flex flex-col gap-4 lg:gap-[10rem] my-[10rem]">
+        <div className="flex flex-col items-center md:items-none gap-4 lg:gap-[10rem] my-[10rem]">
             <div className="flex flex-col lg:flex-row justify-between gap-4 lg:gap-10 mx-3">
                 <div className="flex flex-col gap-4 lg:gap-0">
                     <div className="relative w-[100%] md:w-[48%]">
@@ -28,10 +26,10 @@ const ProjectList = () => {
                     </div>
                     <div className="relative w-[100%] flex justify-end">
                         <div data-scroll data-scroll-speed="0.2" className="bg-[#141414] w-[100%] md:w-[48%] p-[1rem] flex flex-col gap-2">
-                            <p className="text-[#777] text-[12px]">Frontend Development</p>
-                            <img className="h-[30vh] lg:h-[20vh] xl:h-[30vh] object-cover" src={docyardMockupImg} alt="project-img" />
-                            <h1 className="text-[18px] text-[#ddd]">Docyard.co.in</h1>
-                            <p className="text-[11px] text-[#666] text-left">Docyard is a leading agency specializing in efficient document management and collaboration. It offers secure, user-friendly tools for storing, editing, and sharing documents, streamlining workflows for teams. Tailored for various industries, Docyard enhances productivity and ensures seamless teamwork.</p>
+                            <p className="text-[#777] text-[12px]">Website Development</p>
+                            <img className="h-[30vh] lg:h-[20vh] xl:h-[30vh] object-cover" src={blogMockupImg} alt="project-img" />
+                            <h1 className="text-[18px] text-[#ddd]">Personal Blog Portfolio</h1>
+                            <p className="text-[11px] text-[#666] text-left">Holesail.io is a platform designed to simplify Peer-to-Peer (P2P) app development by leveraging cutting-edge technologies like Holepunch and Hyperswarm. It empowers developers to build decentralized applications with ease, enabling secure and efficient real-time communication without relying on central servers.</p>
                         </div>
                     </div>
                 </div>
@@ -58,11 +56,11 @@ const ProjectList = () => {
                         </div>
                     </div>
                     <div className="relative w-[100%] flex justify-end">
-                        <div data-scroll data-scroll-speed="0.2" className="bg-[#141414] w-[100%] md:w-[48%] p-[1rem] flex flex-col gap-2">
-                            <p className="text-[#777] text-[12px]">Website Development</p>
-                            <img className="h-[30vh] lg:h-[20vh] xl:h-[30vh] object-cover" src={blogMockupImg} alt="project-img" />
-                            <h1 className="text-[18px] text-[#ddd]">Personal Blog Portfolio</h1>
-                            <p className="text-[11px] text-[#666] text-left">Holesail.io is a platform designed to simplify Peer-to-Peer (P2P) app development by leveraging cutting-edge technologies like Holepunch and Hyperswarm. It empowers developers to build decentralized applications with ease, enabling secure and efficient real-time communication without relying on central servers.</p>
+                    <div data-scroll data-scroll-speed="0.2" className="bg-[#141414] w-[100%] md:w-[48%] p-[1rem] flex flex-col gap-2">
+                            <p className="text-[#777] text-[12px]">Frontend Development</p>
+                            <img className="h-[30vh] lg:h-[20vh] xl:h-[30vh] object-cover" src={docyardMockupImg} alt="project-img" />
+                            <h1 className="text-[18px] text-[#ddd]">Docyard.co.in</h1>
+                            <p className="text-[11px] text-[#666] text-left">Docyard is a leading agency specializing in efficient document management and collaboration. It offers secure, user-friendly tools for storing, editing, and sharing documents, streamlining workflows for teams. Tailored for various industries, Docyard enhances productivity and ensures seamless teamwork.</p>
                         </div>
                     </div>
                 </div>
@@ -83,9 +81,9 @@ const ProjectList = () => {
                     <div data-scroll data-scroll-speed="0.1" className="relative w-[100%] flex justify-end">
                         <div data-scroll data-scroll-speed="0.2" className="bg-[#141414] w-[100%] md:w-[68%] p-[1rem] flex flex-col gap-2">
                             <p className="text-[#777] text-[12px]">P2P App Development</p>
-                            <img className="h-[30vh] lg:h-[20vh] xl:h-[40vh] object-cover" src={blogMockupImg} alt="project-img" />
+                            <img className="h-[30vh] lg:h-[20vh] xl:h-[40vh] object-cover" src={P2PTextMockupImg} alt="project-img" />
                             <h1 className="text-[18px] text-[#ddd]">P2P Text App</h1>
-                            <p className="text-[11px] text-[#666] text-left">Holesail.io is a platform designed to simplify Peer-to-Peer (P2P) app development by leveraging cutting-edge technologies like Holepunch and Hyperswarm. It empowers developers to build decentralized applications with ease, enabling secure and efficient real-time communication without relying on central servers.</p>
+                            <p className="text-[11px] text-[#666] text-left">P2P Text App is a decentralized chat platform built using Holepunch, Hyperswarm, and web technologies. It goes beyond messaging with features like audio sharing, polls, a whiteboard, and weather updates. Designed for seamless and secure peer-to-peer interaction, it showcases Rohan's innovative approach to P2P networking.</p>
                         </div>
                     </div>
                 </div>
@@ -94,9 +92,32 @@ const ProjectList = () => {
                     <div className="relative h-[100%]">
                         <div className="bg-[#141414] p-[1rem] flex flex-col gap-2">
                             <p className="text-[#777] text-[12px]">P2P App Development</p>
-                            <img className="h-[30vh] md:h-[60vh] object-cover" src={deployMockupImg} alt="project-img" />
+                            <img className="h-[30vh] md:h-[60vh] object-cover" src={P2PTaskMockupImg} alt="project-img" />
                             <h1 className="text-[18px] text-[#ddd]">P2P Task Manager</h1>
-                            <p className="text-[11px] text-[#666] text-left">Holesail.io is a platform designed to simplify Peer-to-Peer (P2P) app development by leveraging cutting-edge technologies like Holepunch and Hyperswarm. It empowers developers to build decentralized applications with ease, enabling secure and efficient real-time communication without relying on central servers.</p>
+                            <p className="text-[11px] text-[#666] text-left">P2P Task Manager is a decentralized platform built using Holepunch, Hyperswarm, and web technologies. It enables users to create, assign, and track tasks in a secure peer-to-peer environment. With innovative features and seamless functionality, it reflects my expertise in building efficient P2P solutions.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-[98%] flex flex-col lg:flex-row justify-between gap-4 lg:gap-10 mx-3">
+                <div className="flex flex-col gap-4">
+                    <div data-scroll data-scroll-speed="0.1" className="relative w-[100%] flex justify-end">
+                        <div data-scroll data-scroll-speed="0.2" className="bg-[#141414] w-[100%] md:w-[68%] p-[1rem] flex flex-col gap-2">
+                            <p className="text-[#777] text-[12px]">P2P App Development</p>
+                            <img className="h-[30vh] lg:h-[20vh] xl:h-[40vh] object-cover" src={P2PImg} alt="project-img" />
+                            <h1 className="text-[18px] text-[#ddd]">P2P Clip App</h1>
+                            <p className="text-[11px] text-[#666] text-left">P2P Clip App is a decentralized clipboard-sharing platform built using Holepunch, Hyperswarm, and web technologies. It allows users to share text and data across devices securely and efficiently in a peer-to-peer environment, showcasing my innovative approach to practical P2P solutions.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="relative h-[100%]">
+                        <div className="bg-[#141414] p-[1rem] flex flex-col gap-2">
+                            <p className="text-[#777] text-[12px]">P2P App Development</p>
+                            <img className="h-[30vh] md:h-[60vh] object-cover" src={holesailGoImg} alt="project-img" />
+                            <h1 className="text-[18px] text-[#ddd]">Holesail-go</h1>
+                            <p className="text-[11px] text-[#666] text-left">Holesail Go is a client for connecting to holesail servers. It supports scanning QR code and is available on the Play Store. Simply download the app from the Play Store, and you are good to go.</p>
                         </div>
                     </div>
                 </div>
